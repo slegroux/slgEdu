6ab494a2e82a14bd815b1359ace16824612fd154
Sylvain Le Groux <sylvainlg@voicera.ai>
2018-12-03 11:18:32 -0800

    update transforms

--- librosa_transforms.py
+++ librosa_transforms.py
@@ -7,6 +7,9 @@ from IPython import embed
 import torch
 from matplotlib import pyplot as plt
 import numpy as np
+from PIL import Image
+from torchvision.transforms import ToTensor
+
 
 class MFCC(object):
     def __init__(self, sr, hop_length=512, n_mfcc=13):
@@ -19,8 +22,11 @@ class MFCC(object):
         audio, label = sample['X'], sample['y']
         audio = audio.numpy()
         #.reshape(audio.shape[1])
-        mfcc = librosa.feature.mfcc(y=audio, sr=self.sr, hop_length=self.hop_length, n_mfcc=self.n_mfcc)
-        mfcc = torch.from_numpy(mfcc)
+        mfcc = librosa.feature.mfcc(y=audio,
+                                    sr=self.sr,
+                                    hop_length=self.hop_length,
+                                    n_mfcc=self.n_mfcc)
+        mfcc = torch.from_numpy(mfcc).double()
         sample = { 'X': mfcc, 'y': label}
         return sample
 
@@ -35,8 +41,11 @@ class MelSpectrogram(object):
         # sample['X'] is a tensor
         audio, label = sample['X'], sample['y']
         audio = audio.numpy()
-        melspec = librosa.feature.melspectrogram(y=audio, sr=self.sr, hop_length=self.hop_length, n_mels=self.n_mels)
-        melspec = torch.from_numpy(melspec)
+        melspec = librosa.feature.melspectrogram(y=audio,
+                                                 sr=self.sr,
+                                                 hop_length=self.hop_length,
+                                                 n_mels=self.n_mels)
+        melspec = torch.from_numpy(melspec).double()
         sample = { 'X': melspec, 'y': label}
         return sample
 
@@ -55,7 +64,7 @@ class Spectrogram(object):
         audio, label = sample['X'], sample['y']
         audio = audio.numpy()
         specgram = librosa.stft(audio, self.n_fft, self.hop_length)
-        specgram = torch.from_numpy(specgram)
+        specgram = torch.from_numpy(specgram).double()
         sample = { 'X': specgram, 'y': label}
         return sample
 
@@ -71,7 +80,7 @@ class LogPowerSpectrogram(object):
         audio = audio.numpy()
         #.reshape(audio.shape[1])
         logspec = librosa.core.amplitude_to_db(np.abs(librosa.stft(audio, n_fft=self.n_fft, hop_length=self.hop_length))**2)
-        logspec = torch.from_numpy(logspec)
+        logspec = torch.from_numpy(logspec).double()
         sample = { 'X': logspec, 'y': label}
         return sample
 
@@ -91,8 +100,27 @@ class PadTrim(object):
         elif self.max_len < len(audio):
             audio = audio[:self.max_len]
             
-        sample = {'X': torch.from_numpy(audio), 'y': label}
+        sample = {'X': torch.from_numpy(audio).double(), 'y': label}
         return sample
 
     def __repr__(self):
         return self.__class__.__name__ + '(max_len={0}, fill_value={1})'.format(self.max_len, self.fill_value)
+
+
+class toPILtoTensor(object):
+    def __init__(self):
+        self.pil2t = ToTensor()
+
+    def __call__(self, sample):
+        spectrum, label = sample['X'], sample['y']
+        spectrum = spectrum.numpy()
+        im = Image.fromarray(spectrum)
+        spectrum_tensor = self.pil2t(im)
+        sample = {'X': spectrum_tensor, 'y': label}
+        return sample
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(max_len={0}, fill_value={1})'.format(self.max_len, self.fill_value)
+
+
+
